40dbafad6dca7d4c2c6a6f6a2f1e32425948fbc6
Tristan <voyanui0697@gmail.com>
2017-12-10 15:17:02 +0100

    .

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -6,6 +6,7 @@
 package jeu;
 
 import java.math.*;
+import java.util.Scanner;
 import javax.swing.*;
 
 
@@ -22,10 +23,37 @@ public class Jeu extends JFrame {
         
         int plateau[][] = new int[longu][larg];
         remplir(plateau, longu);
-        affiche(plateau, longu);
         
+        int pion=0;
+        Scanner input = new Scanner(System.in);
+        int avc=0;
+        affiche(plateau, longu, pion);
         
-        
+        do {
+            
+            System.out.println("De combien de cases voulez-vous avancer ? (entre 1 et 6)");
+            avc = input.nextInt();
+            while (avc<1 || avc>6){
+                System.out.println("ENTRE 1 ET 6");
+                avc = input.nextInt();
+            }
+            
+            pion+=avc;
+            System.out.println("En route vers la case "+(pion+1)+" !");
+            
+            if (plateau[pion][0]!=0){
+                if (plateau[pion][0]>0) 
+                    System.out.println("Case chance ! Tu avances de "+plateau[pion][0]+" cases !");
+                else
+                    System.out.println("Pas de pot ! Tu recules de "+(plateau[pion][0]*-1)+" cases !");
+                pion+=plateau[pion][0];
+            }
+            
+            System.out.println("Arrivee a la case "+(pion+1));
+            
+            affiche(plateau, longu, pion);
+            avc=0;
+        }while (pion < 99);
         
     }
     
@@ -46,21 +74,21 @@ public class Jeu extends JFrame {
             //ajout des pieges et bonus
             
             alead=Math.random()*100;
+            //10% de chances d'avoir un bonus/malus sur chaque case
             if (alead>90&&alead<100){
+                //verification pour ne pas avoir de hors limites
                 while (alead>i ){
                     alead=Math.random()*100;
                 }
                 
                 aleai= (int) alead;
                 
+                //1 chance sur 2 d'avoir un bonus, 1/2 malus
                 alead=Math.random();
                 if (alead<0.5)
                     tab[i][0]=aleai;
                 else
                     tab[i][0]=aleai*-1;
-                
-                
-                
             }
             
         }
@@ -70,20 +98,29 @@ public class Jeu extends JFrame {
         
     }
     
-    public static void affiche(int tab[][], int x) {
+    public static void affiche(int tab[][], int x, int plac) {
         int cpt=0;
+        String aff="";
         for (int j=0; j<10; j++) {
             for (int i=0; i<10; i++) {
-                if (cpt<9)
-                    System.out.print("   "+tab[cpt][1]+" |");
+                aff=" ";
+                if (plac==cpt)
+                    aff+="^";
                 else
-                    System.out.print("  "+tab[cpt][1]+" |");
-                
+                    aff+=" ";
+                if (cpt<9)
+                    aff+=" "+tab[cpt][1]+" |";
+                else
+                    aff+=tab[cpt][1]+" |";
+                System.out.print(aff);
                 cpt++;
             }
+            
+            
+            //affichage des bonus/malus (debug only)
+            
             System.out.print("\n");
             cpt=cpt-10;
-                
             for (int i=0; i<10; i++) {
                 if (tab[cpt][0]<=9 && tab[cpt][0]>=0)
                     System.out.print("   "+tab[cpt][0]+" |");
@@ -93,6 +130,7 @@ public class Jeu extends JFrame {
                     System.out.print(" "+tab[cpt][0]+" |");
                 cpt++;
             }
+            
             System.out.print("\n");
         }
     }
